e583f15b217b0f6d0bce49c93776a733b7f867f8
tombe <mail@thomas-ruff.de>
2024-01-22 22:57:02 +0100

    work in progress

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.card import MDCard
 
 
-class MainApp(MDApp):
-    def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "BlueGray"
-        return Builder.load_file('login.kv')
+class LoginScreen(MDCard):
 
     def login(self):
         self.root.ids.welcome_label.text = f"Welcome {self.root.ids.user.text}"
@@ -15,6 +13,15 @@ class MainApp(MDApp):
         self.root.ids.welcome_label.text = "Welcome"
         self.root.ids.user.text = ""
         self.root.ids.password.text = ""
+
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return LoginScreen()
+
 
 
 MainApp().run()
